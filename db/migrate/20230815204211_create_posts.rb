b7645e721ce5cc9ff0813754449e6bce4870d5f9
Ezra <ezraermy@gmail.com>
2023-08-15 23:52:30 +0300

    Create tables and migrate

---
+++ db/migrate/20230815204211_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.integer :commentsCounter
+      t.integer :likesCounter
+      t.references :author, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
