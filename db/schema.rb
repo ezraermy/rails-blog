f78d945e5f8ef6d78fb68dc1c38d496bec1f8193
Ezra <ezraermy@gmail.com>
2023-08-16 00:53:48 +0300

    Fix table name to snake case convention

--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_161436) do
-    # These are extensions that must be enabled in order to support this database
-    enable_extension "plpgsql"
-  
-    create_table "comments", force: :cascade do |t|
-      t.bigint "author_id", null: false
-      t.bigint "post_id", null: false
-      t.text "text"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["author_id"], name: "index_comments_on_author_id"
-      t.index ["post_id"], name: "index_comments_on_post_id"
-    end
-  
-    create_table "likes", force: :cascade do |t|
-      t.bigint "author_id", null: false
-      t.bigint "post_id", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["author_id"], name: "index_likes_on_author_id"
-      t.index ["post_id"], name: "index_likes_on_post_id"
-    end
-  
-    create_table "posts", force: :cascade do |t|
-      t.string "title"
-      t.text "content"
-      t.integer "commentsCounter"
-      t.integer "likesCounter"
-      t.bigint "author_id", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["author_id"], name: "index_posts_on_author_id"
-    end
-  
-    create_table "users", force: :cascade do |t|
-      t.string "name"
-      t.string "photo"
-      t.text "bio"
-      t.integer "postCounter"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-    end
-  
-    add_foreign_key "comments", "posts"
-    add_foreign_key "comments", "users", column: "author_id"
-    add_foreign_key "likes", "posts"
-    add_foreign_key "likes", "users", column: "author_id"
-    add_foreign_key "posts", "users", column: "author_id"
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_204354) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "author_id"
+    t.bigint "post_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
-  
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "author_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
+end
