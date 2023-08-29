5aa9895533651b6f14fa3dff00926aa1c5edbd00
Ezra <ezraermy@gmail.com>
2023-08-30 00:56:09 +0300

    Install devise and create a setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index
     @user = User.includes(:posts).find(params[:user_id])
     @posts = @user.posts.includes(:comments)
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  devise_for :users
+
   root 'users#index'
 
   resources :users, only: [:index, :show] do
