6f5c39e219d6e18a158aff5b169d64c83abc0425
Ezra <ezraermy@gmail.com>
2023-08-29 19:03:36 +0300

    Separate index and show file for users and posts

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,40 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :feature do
-  describe 'User index page' do
-    before(:each) do
-      @user = User.create(name: 'Tom',
-                          photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                          bio: 'Teacher from Mexico.')
-      visit users_path
-    end
-
-    it 'Redirected to the user show page when clicking on the username' do
-      User.all.each do |user|
-        click_link('Tom')
-        expect(page).to have_current_path(user_path(user.id))
-      end
-    end
-
-    it 'shows the username of all other users' do
-      User.all.each do |_user|
-        expect(page).to have_content('Tom')
-      end
-    end
-
-    it 'shows the number of posts each user has written' do
-      User.all.each do |_user|
-        expect(page).to have_content('Number of posts: 0')
-      end
-    end
-
-    it 'shows the profile picture for each user' do
-      User.all.each do |_user|
-        expect(page).to have_css('img')
-      end
-    end
-  end
-
   describe 'User show page' do
     before(:each) do
       @user = User.create(name: 'Buchu',
